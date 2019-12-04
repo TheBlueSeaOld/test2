d9133dca6f7632f460ca31153569aec4fc0edd52
oldman <379533792@qq.com>
2019-12-04 20:34:55 +0800

    first insert

--- App/Home/Controller/GitController.class.php
+++ App/Home/Controller/GitController.class.php
@@ -5,12 +5,10 @@ use Gregwar\Captcha\CaptchaBuilder;
 class GitController{
 	public function index()
 	{
-		// require "./vendor/autoload.php";
 		$builder = new CaptchaBuilder;
 		$builder->build();
-		
-header('Content-type: image/jpeg');
-$builder->output();
+		header('Content-type: image/jpeg');
+		$builder->output();
 	}
 }
 
